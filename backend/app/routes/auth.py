fcb5ef276b136bbf06b91ee70494f5603f5e0fa7
shiv-jhajharia <shiv454546@gmail.com>
2026-04-17 13:15:50 +0530

    Update auth.py

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -25,6 +25,7 @@ async def google_login(data: GoogleToken):
             requests.Request(),
             GOOGLE_CLIENT_ID
         )
+        console.log(data.token)
 
         # ✅ Security check
         if idinfo["aud"] != GOOGLE_CLIENT_ID:
